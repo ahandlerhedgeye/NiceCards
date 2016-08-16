6a241c21e0efaa9472ae18f3aac2bda613bbfbc5
Alex Handler <ahandler@hedgeye.com>
2016-08-16 13:27:53 -0400

    first test with travis. nonsense test to see if hook is working

---
+++ tests/checkMain.js
@@ -0,0 +1,6 @@
+describe('check that cards loaded in', function () {
+  var main = document.getElementById('main')
+  it('should have card children', function(){
+    expect(d.nodeName).toBe('DIV')
+  })
+})
